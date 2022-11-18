0324209ce8877874cb954cb1462485e5e29a0ffc
Bello <alfulaani1345@gmail.com>
2022-11-18 09:44:26 +0100

    Changing Names

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Logo from '../assets/icon.png';
 const Footer = () => {
   return (
     <div className="w-full h-[3rem] shadow-md text-center justify-center flex fixed bottom-0 m-auto bg-slate-500">
-        <div className=" items-center">
+        <div className="flex items-center">
             <span className="text-white text-[18px]">
                 Developed by <span className='hover:text-blue-600'> <a href="https://github.com/Professor1345">Bello</a> </span>
             </span>
